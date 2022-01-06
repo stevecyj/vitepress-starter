0cdfe2f7c86db6eee5869bd0dc10925a26f44a5c
steve_tsao <steve_tsao@neithnet.con>
2022-01-06 16:26:22 +0800

    1st commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   title: "go VitePress",
+//   description: "Just playing around.",
+// };
+
+const config = {
+  title: "1st",
+  description: "Just playing around.",
+};
+
+export default config;
